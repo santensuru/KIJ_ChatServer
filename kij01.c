0d827ea56e68bbdb095efd399ef4fa1f89abfbee
santensuru <djuned.ong@gmail.com>
2015-03-08 12:27:44 +0700

    broadcast list, fixed bug -> iterator cek_IP

--- kij01.c
+++ kij01.c
@@ -9,6 +9,8 @@
 #include <stdlib.h>
 #include <pthread.h>
 
+int lastcli = -1;
+
 struct user {
     int sockcli;
     char ip_active[16];
@@ -45,7 +47,7 @@ void pop(int cli) {
         free(temp);
     } else {
         do {
-            if (temp2->sockcli = cli) {
+            if (temp2->sockcli == cli) {
                 temp->next = temp2->next;
                 free(temp2);
             } else {
@@ -58,13 +60,14 @@ void pop(int cli) {
 }
 
 int cek_IP(char *msg) {
-     temp = awal;
-     do {
-         if (strcmp(temp->ip_active, msg) == 0) {
-             return temp->sockcli;
-         }
-     } while (temp->next != NULL);
-     return -1;
+    temp = awal;
+    do {
+       if (strcmp(temp->ip_active, msg) == 0) {
+           return temp->sockcli;
+       }
+       temp = temp->next;
+    } while (temp != NULL);
+    return -1;
 }
 
 void send_who(int dest) {
@@ -80,12 +83,9 @@ void send_who(int dest) {
         strcat(msg_temp, "\r\n");
         write(dest, msg_temp, strlen(msg_temp));
         fflush(stdout);
-        if (temp->next != NULL) {
-            temp = temp->next;
-        } else {
-            break;
-        }
-    } while (1);
+        temp = temp->next;
+    } while (temp != NULL);
+    
     strcpy(msg_temp, "END.\r\n");
     write(dest, msg_temp, strlen(msg_temp));
     fflush(stdout);
@@ -96,6 +96,38 @@ typedef struct haha {
     int sockcli;
     char ip_active[16];
 } haha;
+
+void *broadcast(void *ptr) {
+    haha * handler = (haha *)ptr;
+    send_who(handler->sockcli);
+}
+
+void broadcast_IP() {
+     // thread acc //
+    void *join;
+     
+    pthread_t broadcast_t;
+    int broadcast_i;
+	pthread_mutex_t broadcast_m = PTHREAD_MUTEX_INITIALIZER;
+    
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    // end //
+    
+    haha *handler = (haha *) malloc( sizeof ( haha ) );
+    
+    int i;
+    for (i = 4; i <= lastcli; i++) {
+        handler->sockcli = i;
+        
+        pthread_mutex_lock( &broadcast_m );
+        broadcast_i = pthread_create( &broadcast_t, &attr, broadcast, (void *)handler);
+        
+        pthread_mutex_unlock( &broadcast_m );
+        
+        pthread_join(broadcast_t, &join);
+    }
+}
 
 /*
 char user[100][16];
@@ -147,10 +179,11 @@ void *acc(void *ptr) {
     
     /* here */
     
-    
-    strcpy(msg_send, "WELCOME :)\r\n");
+    strcpy(msg_send, "HAY. :) >> Welcome.... <<\r\n");
     write(handler->sockcli, msg_send, strlen(msg_send));
     fflush(stdout);
+    
+    broadcast_IP();
     
     while(1) {
         bzero(msg, 255);
@@ -177,6 +210,7 @@ void *acc(void *ptr) {
             sscanf(msg, "USER %s %[^\r\n]", comment, msg_send);
             if (strcmp(comment, "") == 0) {
                 sprintf(msg_send, "IP?\r\n");
+                
             } else {
                 dest = cek_IP(comment);
                 if (dest > -1) {
@@ -187,124 +221,136 @@ void *acc(void *ptr) {
                         strcat(msg_temp, ": ");
                         strcat(msg_send, "\r\n");
                         write(dest, msg_temp, strlen(msg_temp));
+                        fflush(stdout);
                         write(dest, msg_send, strlen(msg_send));
                         fflush(stdout);
-                        bzero(msg_send, 4096);
+                        strcpy(msg_send, "SEND\r\n");
+                        
+                    } else {
+                        sprintf(msg_send, "USER message for IP = %s ?\r\n", comment);
+                    
                     }
+                    
                 } else {
                     sprintf(msg_send, "USER with IP = %s not found\r\n", comment);
+                    
                 }
             }
-        }
+            
+        } else if (strstr(msg, "WHO?") != NULL) {
+            send_who(handler->sockcli);
+            bzero(msg_send, 4096);
+            
+        } else if (strstr(msg, "OUT.") != NULL) {
+            strcpy(msg_send, "BYE. :) >> Thank You.... <<");
+            write(handler->sockcli, msg_send, strlen(msg_send));
+            fflush(stdout);
+            pop(handler->sockcli);
+            break;
+            
+        } else {
+            strcpy(msg_send, "???. @x@ >> Commend not recognized.... <<\r\n");
         
-        else if (strstr(msg, "WHO?") != NULL) {
-             send_who(handler->sockcli);
-             bzero(msg_send, 4096);
-        }
-        
-        else if (strstr(msg, "OUT.") != NULL) {
-             strcpy(msg_send, "BYE. :)");
-             write(handler->sockcli, msg_send, strlen(msg_send));
-             fflush(stdout);
-             pop(handler->sockcli);
-             break;
         }
         
         write(handler->sockcli, msg_send, strlen(msg_send));
         fflush(stdout);
     }
-        
+    
     close(handler->sockcli);
-        
+    
     return;
 }
 
 void main()
 {
-     awal = NULL;
-     
-     int sockfd = 0, sockcli = 0;
-     int retval;
-     struct sockaddr_in servaddr, cliaddr;
-     
-     sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-     
-     bzero(&servaddr, sizeof(servaddr));
-     
-     servaddr.sin_family = AF_INET;
-     servaddr.sin_port = htons(6060);
-     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-     retval = bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
-     
-     if (retval <0)
-     {
-                perror(strerror(errno));
-                exit(-1);
-     }
-     
-     printf("Server mengikat port 6060\n");
-     retval = listen(sockfd, 5);
-     printf("Server menunggu panggilan...\n");
-     bzero(&cliaddr, sizeof(cliaddr));
-     socklen_t clisize = sizeof(cliaddr);
-     
-     // thread acc //
+    awal = NULL;
+    
+    int sockfd = 0, sockcli = 0;
+    int retval;
+    struct sockaddr_in servaddr, cliaddr;
+    
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    bzero(&servaddr, sizeof(servaddr));
+    
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(6060);
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    retval = bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+    
+    if (retval < 0)
+    {
+        perror(strerror(errno));
+        exit(-1);
+    }
+    
+    printf("Server mengikat port 6060\n");
+    retval = listen(sockfd, 5);
+    printf("Server menunggu panggilan...\n");
+    bzero(&cliaddr, sizeof(cliaddr));
+    socklen_t clisize = sizeof(cliaddr);
+    
+    // thread acc //
     void *join;
-     
-     pthread_t acc_t;
+    
+    pthread_t acc_t;
     int acc_i;
-	pthread_mutex_t acc_m = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t acc_m = PTHREAD_MUTEX_INITIALIZER;
     
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     // end //
-	
+    
     while(sockcli = accept(sockfd, (struct sockaddr*)&cliaddr , &clisize)) {
-     
+        
         haha *handler = (haha *) malloc( sizeof ( haha ) );
-	    printf("sockcli --> %d\n", sockcli);
-	    printf("Ada klien masuk dari %s %d\n", inet_ntoa(cliaddr.sin_addr), (int) ntohs(cliaddr.sin_port));
-	    
-	    //strcpy(user[sockcli], inet_ntoa(cliaddr.sin_addr));
-	    
-	    //printf("%d %s\n", sockcli, user[sockcli]);
-	    
-	    push(sockcli, inet_ntoa(cliaddr.sin_addr), "anonymous");
-	    
-	    char str[INET_ADDRSTRLEN];
-	    inet_ntop(AF_INET, &(cliaddr.sin_addr), str, INET_ADDRSTRLEN);
-	    printf("%s\n", str);
-	    
-	    struct sockaddr_in localAddress;
-	    socklen_t addressLength = sizeof(localAddress);
-	    getsockname(sockcli, (struct sockaddr*)&localAddress, &addressLength);
-	    
-	    char ip_active[16];
-	    strcpy(ip_active, inet_ntoa( localAddress.sin_addr));
-	    printf("local address: %s\n", ip_active);
-	    printf("local port: %d\n", (int) ntohs(localAddress.sin_port));
- 	   
-         
-         if (sockcli < 0)
-         {
-                     perror(strerror(errno));
-                     exit(-1);
-         }
-         
-     // baca dan tulis pesan disini
-     
-     handler->sockcli = sockcli;
-	    printf("%d\n", handler->sockcli);
-	    strcpy(handler->ip_active, ip_active);
-    	
-	    pthread_mutex_lock( &acc_m );
-		acc_i = pthread_create( &acc_t, &attr, acc, (void *)handler);
-		
-		pthread_mutex_unlock( &acc_m );
-		
-		//pthread_join(acc_t, &join);
-	}
-     close(sockfd);           
+        printf("sockcli --> %d\n", sockcli);
+        printf("Ada klien masuk dari %s %d\n", inet_ntoa(cliaddr.sin_addr), (int) ntohs(cliaddr.sin_port));
+        
+        //strcpy(user[sockcli], inet_ntoa(cliaddr.sin_addr));
+        
+        //printf("%d %s\n", sockcli, user[sockcli]);
+        
+        push(sockcli, inet_ntoa(cliaddr.sin_addr), "anonymous");
+        
+        if (lastcli < sockcli)
+            lastcli = sockcli;
+        
+        char str[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &(cliaddr.sin_addr), str, INET_ADDRSTRLEN);
+        printf("%s\n", str);
+        
+        struct sockaddr_in localAddress;
+        socklen_t addressLength = sizeof(localAddress);
+        getsockname(sockcli, (struct sockaddr*)&localAddress, &addressLength);
+        
+        char ip_active[16];
+        strcpy(ip_active, inet_ntoa( localAddress.sin_addr));
+        printf("local address: %s\n", ip_active);
+        printf("local port: %d\n", (int) ntohs(localAddress.sin_port));
+        
+        
+        if (sockcli < 0)
+        {
+            perror(strerror(errno));
+            exit(-1);
+        }
+        
+        // baca dan tulis pesan disini
+        
+        handler->sockcli = sockcli;
+        printf("%d\n", handler->sockcli);
+        strcpy(handler->ip_active, ip_active);
+        
+        pthread_mutex_lock( &acc_m );
+        acc_i = pthread_create( &acc_t, &attr, acc, (void *)handler);
+        
+        pthread_mutex_unlock( &acc_m );
+        
+        //pthread_join(acc_t, &join);
+    }
+    close(sockfd);           
 }
  
